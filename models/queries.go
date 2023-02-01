b4836d6999d8c8aa83f8c5d056179fe0f75e2234
Diogo Braz <diogo.braz04@gmail.com>
2023-02-01 10:32:02 -0300

    feat(models): add get all todos query

--- models/queries.go
+++ models/queries.go
@@ -29,3 +29,29 @@ func GetTodo(id int64) (todo Todo, err error) {
 
 	return
 }
+
+func GetAll() (todos []Todo, err error) {
+	conn, err := database.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query(`SELECT id, title, description, done FROM todos`)
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		var todo Todo
+
+		err = rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+		if err != nil {
+			continue
+		}
+
+		todos = append(todos, todo)
+	}
+
+	return
+}
