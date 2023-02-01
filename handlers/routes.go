8e6c00185c8490000c26ccbfbce2fb4c5783f01a
Diogo Braz <diogo.braz04@gmail.com>
2023-02-01 11:29:23 -0300

    feat(handler): add delete todo handler

--- handlers/routes.go
+++ handlers/routes.go
@@ -77,3 +77,31 @@ func UpdateTodo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(resp)
 }
+
+func DeleteTodo(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Erro ao converter o id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.DeleteTodo(int64(id))
+	if err != nil {
+		log.Printf("Erro ao deletar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Erro: foram removidos %d registros", rows)
+	}
+
+	resp := map[string]any{
+		"Error":   false,
+		"Message": "registro removido com sucesso!",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
