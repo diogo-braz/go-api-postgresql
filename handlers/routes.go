21867bf95e0755c1e42ece7e8b283cb8171504ce
Diogo Braz <diogo.braz04@gmail.com>
2023-02-01 11:18:13 -0300

    feat(handler): add update todo handler

--- handlers/routes.go
+++ handlers/routes.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/diogo-braz/go-api-postgresql/models"
+	"github.com/go-chi/chi"
 )
 
 func CreateTodo(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +35,43 @@ func CreateTodo(w http.ResponseWriter, r *http.Request) {
 			"Error":   false,
 			"Message": fmt.Sprintf("Todo inserido com sucesso! ID: %d", id),
 		}
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
+
+func UpdateTodo(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Erro ao converter o id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	var todo models.Todo
+
+	err = json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.UpdateTodo(int64(id), todo)
+	if err != nil {
+		log.Printf("Erro ao atualizar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Erro: foram atualizados %d registros", rows)
+	}
+
+	resp := map[string]any{
+		"Error":   false,
+		"Message": "dados foram atualizados com sucesso!",
 	}
 
 	w.Header().Add("Content-Type", "application/json")
