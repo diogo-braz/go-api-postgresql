6907c48f07722268586f64c5367506639f51bedf
Diogo Braz <diogo.braz04@gmail.com>
2023-02-01 11:33:25 -0300

    feat(handler): add get todo handler

--- handlers/routes.go
+++ handlers/routes.go
@@ -115,3 +115,22 @@ func ListTodos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(todos)
 }
+
+func GetTodo(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Erro ao converter o id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	todo, err := models.GetTodo(int64(id))
+	if err != nil {
+		log.Printf("Erro ao atualizar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todo)
+}
